4ab1f1d60215bcc87609475b60b8bf522a29c720
Brachamul <barnaby.brachamul@gmail.com>
2019-01-21 13:06:22 +0100

    formulaire en ligne

---
+++ airtable.js
@@ -0,0 +1,48 @@
+const DATABASE_URL = "https://api.airtable.com/v0/appnmJ4kX95S45tEM/"
+const API_KEY = "keyb0Dx3zRgfrZ0hx"
+let headers = {'authorization': 'Bearer ' + API_KEY, 'content-type': 'application/json'}
+
+$.ajaxSetup({headers: headers})
+
+var airtable = {
+  get: function (table, record_id, operation=console.log, extra_context=false) {
+    var url = DATABASE_URL + table + '/' + record_id
+    $.get(url).done(function(response){
+      operation(response, extra_context)
+    }).fail(function(response){
+      console.error(
+        "AirTable GET request has failed!" +
+        "\n Url: " + url +
+        "\n Status: " + response.status +
+        "\n Response: " + response.responseText)
+    })
+  },
+  update: function(table, record_id, data) {
+    $('body').addClass("show-updating")
+    var url = DATABASE_URL + table + '/' + record_id
+    data = {'fields': data}
+    data = JSON.stringify(data)
+    $.ajax({url: url, type: 'PATCH', data: data}).done(function(response){
+      $('body').removeClass("show-updating")
+    }).fail(function(response){
+      console.error(
+        "AirTable UPDATE has failed!" +
+        "\n Url: " + url +
+        "\n Data: " + data +
+        "\n Status: " + response.status +
+        "\n Response: " + response.responseText)
+    })
+  },
+  list: function (table, operation=console.log, params="") {
+    var url = DATABASE_URL + table + '/' + params
+    $.get(url).done(function(response){
+      operation(response)
+    }).fail(function(response){
+      console.error(
+        "AirTable LIST has failed!" +
+        "\n URL: " + url +
+        "\n Status: " + response.status +
+        "\n Response: " + response.responseText)
+    })
+  }
+}
